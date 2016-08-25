cabee254354baed06d22d8642e5c67bd0f6b4735
Hades <lcm_hades@163.com>
2016-08-25 18:32:13 +0800

    update BaseRecyclerViewAdaptor

--- app/src/main/java/com/demo/hades/headerandfooterrecycleviewdemo/MainActivity.java
+++ app/src/main/java/com/demo/hades/headerandfooterrecycleviewdemo/MainActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,33 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        first_rv = (RecyclerView) findViewById(R.id.first_rv);
 
-        first_rv.setItemAnimator(new DefaultItemAnimator());
-        first_rv.setHasFixedSize(true);
-        first_rv.setLayoutManager(new GridLayoutManager(this, 4));
-        // first_rv.setAdapter(new RecyclerViewAdapter(this, ));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+//        List<String> listData = new ArrayList<>();
+//        for (int i=0; i<10; i++){
+//            listData.add("test " + i);
+//        }
+//
+//        first_rv = (RecyclerView) findViewById(R.id.first_rv);
+//
+//        first_rv.setItemAnimator(new DefaultItemAnimator());
+//        first_rv.setHasFixedSize(true);
+//        first_rv.setLayoutManager(new GridLayoutManager(this, 4));
+//
+//        RecyclerViewAdapter adt = new RecyclerViewAdapter(this, listData);
+//        TextView tv = new TextView(this);
+//        tv.setText("kkkkkkkkkkkkkkkkkkkkkk");
+//        adt.addHeaderView(tv);
+//        first_rv.setAdapter(adt);
     }
 }
--- app/src/main/java/com/demo/hades/headerandfooterrecycleviewdemo/RecyclerViewAdapter.java
+++ app/src/main/java/com/demo/hades/headerandfooterrecycleviewdemo/RecyclerViewAdapter.java
@@ -3,32 +3,46 @@ package com.demo.hades.headerandfooterrecycleviewdemo;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Hades on 2016/8/25.
  */
 public class RecyclerViewAdapter extends BaseRecyclerViewAdaptor {
 
+    private Context mContext;
+    private List<String> mListData;
+
+    public RecyclerViewAdapter(Context context, List<String> list){
+        mListData = list;
+        mContext = context;
+    }
 
     @Override
     protected RecyclerView.ViewHolder onCreate(ViewGroup viewGroup, int itemtype) {
+        ViewHolder holder = ViewHolder.createViewHolder(mContext, viewGroup, R.layout.data_item);
 
-        return null;
+        return holder;
     }
 
     @Override
     protected void onRealBindViewHolder(RecyclerView.ViewHolder viewHolder, int pos) {
-
+        TextView tv = ((ViewHolder)viewHolder).getView(R.id.tv);
+        tv.setText("aaaaaaaaaaaaaaaaaaaaaaa");
     }
 
     @Override
     protected int getRealItemCount() {
-        return 0;
+        return mListData == null ? 0 : mListData.size();
     }
 
     @Override
     protected int getRealItemViewType(int pos) {
         return 0;
     }
+
+
 
 }
